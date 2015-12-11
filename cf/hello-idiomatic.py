59e42073c5a5b39b940e469c14c7d2df718285b8
Rich Barlow <disconnector@gmail.com>
2015-12-11 10:54:10 -0500

    Sigh.  I'm an idiot occasionally

--- cf/hello-idiomatic.py
+++ cf/hello-idiomatic.py
@@ -16,7 +16,7 @@ COLOR = GREEN
 
 @app.route('/')
 def hello():
-    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,myname=my_name)
+    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,myname="Rich Barlow")
 
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0', port=env.port)
