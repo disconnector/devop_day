926dcff1ad57c3a71e3bc4b3ce06f5667a6d6a35
Rich Barlow <disconnector@gmail.com>
2015-12-11 15:22:40 -0500

    Fixed app router

--- local_code/hello-idiomatic.py
+++ local_code/hello-idiomatic.py
@@ -2,20 +2,16 @@ from __future__ import print_function
 import os
 import uuid
 from flask import Flask, render_template
-from cfenv import AppEnv
 
-
-env = AppEnv()
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
-se_real_name = "Rich Barlow"
-COLOR = GREEN
+COLOR = BLUE
 
 @app.route('/')
 def hello():
-    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,sename=se_real_name)
+    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,sename="Rich Barlow")
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0', port=env.port)
+    app.run(debug=False,host='0.0.0.0')
