7040f42d02e90edef8e6e5d1ed4eea263d99b33c
Rich Barlow <disconnector@gmail.com>
2015-12-11 15:25:05 -0500

    WOOT!

--- local_code/hello_python.py
+++ local_code/hello_python.py
@@ -3,16 +3,15 @@ import os
 import uuid
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
-se_real_name = "Rich Barlow"
-COLOR = GREEN
+COLOR = BLUE
 
 @app.route('/')
 def hello():
-    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,sename=se_real_name)
+    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,myname="Rich Barlow")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False,host='0.0.0.0')
